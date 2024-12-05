abddb5ed128c42d4b607670a0fb6517401358b6d
Joshua Jenkins <2004joshuaj@gmail.com>
2024-12-04 21:32:46 -0800

    reusable recipe cards

---
+++ recipeapp/src/main/webapp/js/recipeItem.js
@@ -0,0 +1,27 @@
+//function to generate a recipe card that can be displayed in grid, need to include recipeItem.css in html file
+
+const recipeItem = `
+	<div class="recipeItemContainer">
+		<div class="upperSection">
+			<p1 id="title"></p1>
+			<p2 id="category"></p2>
+		</div>
+		<div class="lowerSection">
+			<div class="line"></div>
+			<p3 id="user"></p3>
+		</div>
+	</div>
+`;
+
+function getRecipeItem(title, category, creator) {
+	//creating recipe element
+	const container = document.createElement("div");
+	container.innerHTML = recipeItem;
+	
+	//adding info
+	container.querySelector("#title").textContent = title;
+	container.querySelector("#category").textContent = category;
+	container.querySelector("#user").textContent = "By: " + creator;
+	
+	return container;
+}
