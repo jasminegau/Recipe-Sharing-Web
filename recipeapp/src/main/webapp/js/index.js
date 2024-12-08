ee85528efa4256c3deca188268764db67da8bfe8
ryansanchezzz <ryans368@usc.edu>
2024-12-08 01:34:24 -0800

    small error fix

--- recipeapp/src/main/webapp/js/index.js
+++ recipeapp/src/main/webapp/js/index.js
@@ -1,4 +1,4 @@
-loggedIn();
+
 for (let i = 0; i < 10; i++) {
 	document.getElementById("recipes").appendChild(getRecipeItem("test", "yummy", "test user"));
 }
@@ -15,28 +15,4 @@ function updateSearch(event) {
 		.catch(error => {
 			console.log(error);
 		})
-}
-
-function loggedIn()
-{
-	if (!sessionStorage.getItem("User"))
-	{
-		document.getElementById("login").innerHTML = "Login";
-		document.getElementById("register").innerHTML = "Register";
-		document.getElementById("register").href = "registration.html";
-		document.getElementById("login").href = "Login.html";
-		
-	}
-	else
-	{
-		document.getElementById("login").innerHTML = "Profile";
-		document.getElementById("register").innerHTML = "Sign Out";
-		document.getElementById("login").href = "Profile.html";
-		
-		row.addEventListener('click', function() {
-			sessionStorage.setItem("User", null);
-			loggedIn();
-		}); 
-		document.getElementById("register").href = "javascript:void(0)";
-	}
 }
