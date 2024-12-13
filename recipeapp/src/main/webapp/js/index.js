d38e1f8930f66cef201f449128346d9a3e862c20
ryansanchezzz <ryans368@usc.edu>
2024-12-12 23:38:00 -0800

    final final touches

--- recipeapp/src/main/webapp/js/index.js
+++ recipeapp/src/main/webapp/js/index.js
@@ -1,29 +1,42 @@
+fill();
+function fill()
+{
+	
 
-fetch(`http://localhost:8080/recipeapp/ShowAllRecipe`)
-		.then(response => response.json())
-		.then(data => {
-			document.getElementById("recipes").innerHTML = "";
-			for (let recipe of data) {
-				document.getElementById("recipes").appendChild(getRecipeItem(recipe.id, recipe.title, recipe.category, recipe.author));
-			}
-		})
-		.catch(error => {
-			console.log(error);
-		})
+	fetch(`http://localhost:8080/recipeapp/ShowAllRecipe`)
+			.then(response => response.json())
+			.then(data => {
+				document.getElementById("recipes").innerHTML = "";
+				for (let recipe of data) {
+					document.getElementById("recipes").appendChild(getRecipeItem(recipe.id, recipe.title, recipe.category, recipe.author));
+				}
+			})
+			.catch(error => {
+				console.log(error);
+			})
+	}
 
 function updateSearch(event) {
-	event.preventDefault();
-	console.log(document.getElementById("search").value);
-	const query = document.getElementById("search").value;
-	fetch(`http://localhost:8080/recipeapp/Search?query=${query}`)
-		.then(response => response.json())
-		.then(data => {
-			document.getElementById("recipes").innerHTML = "";
-			for (let recipe of data) {
-				document.getElementById("recipes").appendChild(getRecipeItem(recipe.id, recipe.title, recipe.category, recipe.author));
-			}
-		})
-		.catch(error => {
-			console.log(error);
-		})
+	if (document.getElementById("search").value !== "")
+	{
+		event.preventDefault();
+		console.log(document.getElementById("search").value);
+		const query = document.getElementById("search").value;
+		fetch(`http://localhost:8080/recipeapp/Search?query=${query}`)
+			.then(response => response.json())
+			.then(data => {
+				document.getElementById("recipes").innerHTML = "";
+				for (let recipe of data) {
+					document.getElementById("recipes").appendChild(getRecipeItem(recipe.id, recipe.title, recipe.category, recipe.author));
+				}
+			})
+			.catch(error => {
+				
+				console.log(error);
+			})
+	}
+	else
+	{
+		fill();
+	}
 }
